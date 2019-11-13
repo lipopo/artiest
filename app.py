82bcb933c82e95c3f64ee14b4b99673fc659def0
lipo <lipo8081@gmail.com>
2019-11-13 16:51:29 +0800

    初始化网站部分页面

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 from flask import Flask, jsonify, render_template
+import jinja2
 
 
 # import local package
@@ -10,18 +11,25 @@ from setting import setting
 
 
 pwd_folder = os.path.dirname(__file__)
+template_folder=os.path.join(pwd_folder, 'static')
 app = Flask(
     __name__,
     static_folder=os.path.join(pwd_folder, 'static'),
-    static_url_path='/',
-    template_folder=os.path.join(pwd_folder, 'static')
+    static_url_path='/'
 )
+app.jinja_loader = jinja2.FileSystemLoader(template_folder, encoding='utf-8')
 __version__ = '0.0.1'
 
 
 @app.route('/')
 def index():
     return render_template('html/coming_soon.html')
+
+
+@app.route('/test')
+def test():
+    print(render_template('html/index.html', **setting.website_setting))
+    return render_template('html/index.html', **setting.website_setting)
 
 
 @app.route('/version')
--- setting.py
+++ setting.py
@@ -1,11 +1,24 @@
 import os
 
+import yaml
+
+
+pwd_folder = os.path.dirname(__file__)
+
 
 class Setting(object):
+    _website_setting = {}
+
+
     @property
     def db_url(self):
         return os.environ.get('DATABASE')
 
+    @property
+    def website_setting(self):
+        # if not self._website_setting:
+        self._website_setting = yaml.load(open(os.path.join(pwd_folder, 'website.yml')))
+        return self._website_setting
 
 setting = Setting()
 
